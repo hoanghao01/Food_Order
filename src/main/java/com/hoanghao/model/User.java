48a2d1ed57a6df3d95e3fc2c8f05db7ab9a2da2f
hoanghao01 <haongnguyen6345@gmail.com>
2025-02-19 19:12:13 +0700

    ud1

---
+++ src/main/java/com/hoanghao/model/User.java
@@ -0,0 +1,32 @@
+package com.hoanghao.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String fullName;
+
+    private String email;
+
+    private String password;
+
+    private USER_ROLE role;
+
+    private List<Order> orders = new ArrayList<>();
+
+}
